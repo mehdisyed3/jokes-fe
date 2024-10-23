63e1f692d8522d60e7f1e396458aefa5c630c714
Syed Mehdi <mehdisyed3@gmail.com>
2024-10-23 10:18:41 -0400

    typo fixed

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div className='bg-stone-100 flex flex-col w-3/4 sm:5/6 justify-center items-center py-10 mx-auto'>
       <h1 className='text-3xl sm:text-5xl py-5 text-center'>Welcome to the Jokes App</h1>
-      <p className='text-xl sm:text-3xl py-5 text-center'>Click on the button below to be redirected to the Joke Page</p>
+      <p className='text-xl sm:text-3xl py-5 text-center'>Click on the button below to be redirected to the Jokes Page</p>
       <Link href='/jokes'>
         <button className='bg-gray-200 border-2 rounded-lg cursor-pointer p-4 uppercase font-semibold'>
           Go to Jokes Page
